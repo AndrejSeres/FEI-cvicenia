1d563c27e3dc14d60807368845b5a1d1bb52310e
Andrej Seres <andrej.seres@outlook.sk>
2022-02-23 15:06:21 +0100

    int pole + vyplnenie z klavesnice

--- src/main/java/sk/stuba/oop/App.java
+++ src/main/java/sk/stuba/oop/App.java
@@ -1,7 +1,18 @@
 package sk.stuba.oop;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Ahoj svet");
+        int[] pole = new int[3];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i<pole.length; i++  ){
+            System.out.println("Napis cislo do pola");
+            pole[i]=sc.nextInt();
+        }
+        for(int i = 0; i<pole.length; i++  ){
+            System.out.print(pole[i]+" ");
+        }
     }
 }
